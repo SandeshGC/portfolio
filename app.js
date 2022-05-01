fefcf6e975ca982c92669d8fab3134fb95503110
Sandesh GC <59115123+SandeshGC@users.noreply.github.com>
2022-05-01 12:18:05 +0545

    made changes

---
+++ app.js
@@ -0,0 +1,5 @@
+    const submitBtn = document.getElementById("submit-contact-form");
+    submitBtn.addEventListener('click', function (event){
+        console.log("Button clicked")
+        event.preventDefault();
+    })
